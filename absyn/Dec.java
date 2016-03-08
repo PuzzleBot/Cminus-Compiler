e02aea083f0d6e63768f570259edf1405f3dd5a0
Brandon Tan <tanb@uoguelph.ca>
2016-03-07 19:45:19 -0500

    Resolved conflict

--- absyn/Dec.java
+++ absyn/Dec.java
@@ -1,9 +1,5 @@
 package absyn;
 
-<<<<<<< HEAD
-abstract public class Dec{
-=======
 public abstract class Dec extends Absyn{
->>>>>>> cf25166104d05eb2c42787d52792a5710edb4fdb
 
 }
--- absyn/DecList.java
+++ absyn/DecList.java
@@ -1,14 +1,8 @@
 package absyn;
 
 public class DecList extends Absyn{
-<<<<<<< HEAD
     public Dec head;
     public DecList tail;
-=======
-    Dec head;
-    DecList tail;
->>>>>>> cf25166104d05eb2c42787d52792a5710edb4fdb
-    
     
     public DecList(Dec head,DecList tail){
     this.head=head;
