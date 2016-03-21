ce92050021d8e0aef99c479380c3b84841f1814b
Brandon Tan <tanb@uoguelph.ca>
2016-03-20 21:45:57 -0400

    Expanded code a little

--- src/Identifier.java
+++ src/Identifier.java
@@ -2,8 +2,16 @@ import absyn.*;
 import java.util.*;
 
 public class Identifier{
+    public static final int INT = 1;
+    public static final int INT_ARRAY = 2;
+    public static final int FUNCTION_INT = 3;
+    public static final int FUNCTION_VOID = 4;
+    
+    public String name;
+    public int type;
 
-    public Identifier(){
-        
+    public Identifier(String name, int type){
+        this.name = name;
+        this.type = type;
     }
 }
--- src/SemanticHashmap.java
+++ src/SemanticHashmap.java
@@ -2,8 +2,21 @@ import absyn.*;
 import java.util.*;
 
 public class SemanticHashmap{
+    public Stack<HashMap<Identifier>> hashMapList;
 
     public SemanticHashmap(){
+        hashMapList = new Stack<HashMap<Identifier>>;
+    }
+    
+    public void insert(Identifier newIdentifier){
+        
+    }
+    
+    public Identifier lookup(String identifierName){
+        
+    }
+    
+    public void deleteInnerScope(){
         
     }
 }
